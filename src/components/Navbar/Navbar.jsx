a1aebfbf66d18387c641d03074fb146f973b3813
yashmali02 <yashmali2530@gmail.com>
2026-01-06 11:42:24 +0530

    feat: added styles to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.main_Nav}>
+      <div className={styles.nav_logo}>test website logo</div>
+      <div className={styles.nav_search}>
+        <input placeholder="Enter a movie name" />
+      </div>
+      <div className={styles.nav_right}>
+        <nav>
+          <ul>
+            <li>HOME</li>
+            <li>MOVIES</li>
+            <li>TV Series</li>
+            <li>HOME</li>
+          </ul>
+        </nav>
+        <button className={styles.nav_login} type="button">
+          Login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
