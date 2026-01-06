f6e614b3af6b85b4af55eac64ac97bafb5f65972
yashmali02 <yashmali2530@gmail.com>
2026-01-06 12:21:30 +0530

    fix: conditional rendering to movie posters

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,13 +8,19 @@ const Card = ({ movies }) => {
       {movies.map((movie, idx) => (
         <div key={idx} className={styles.main_card}>
           <h2>{movie?.title}</h2>
-          <img src={movie?.poster} />
-          <div className={styles.year_rating}>
-            <p>{movie.year}</p>
-            <p>{movie.genre}</p>
-            <p>{movie.rating}</p>
+          {movie?.poster ? (
+            <img src={movie?.poster} />
+          ) : (
+            "No Poster available for this movie"
+          )}
+          <div className={styles.year_rating_container}>
+            <div className={styles.year_rating}>
+              <p>{movie.year}</p>
+              <p>{movie.genre}</p>
+              <p>{movie.rating}</p>
+            </div>
+            <button className={styles.watch_btn}>Watch Now</button>
           </div>
-          <button className={styles.watch_btn}>Watch Now</button>
         </div>
       ))}
     </>
